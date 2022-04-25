67da0139cd8b444d40c3f0bb699ae4c81c0ede02
quantrananh2304 <65522530+quantrananh2304@users.noreply.github.com>
2022-04-25 16:04:15 +0700

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,53 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
+	env: {
+		browser: true,
+		es6: true,
+		node: true,
+	},
 
-  // the ts-eslint recommended ruleset sets the parser so we need to set it back
-  parser: 'vue-eslint-parser',
+	// the ts-eslint recommended ruleset sets the parser so we need to set it back
+	parser: 'vue-eslint-parser',
 
-  parserOptions: {
-    ecmaVersion: 2020,
-    parser: '@typescript-eslint/parser',
-    extraFileExtensions: ['.vue'],
-    ecmaFeatures: {
-      jsx: true,
-    },
-    sourceType: 'module',
-  },
+	parserOptions: {
+		ecmaVersion: 2020,
+		parser: '@typescript-eslint/parser',
+		extraFileExtensions: ['.vue'],
+		ecmaFeatures: {
+			jsx: true,
+		},
+		sourceType: 'module',
+	},
 
-  plugins: ['@typescript-eslint'],
+	plugins: ['@typescript-eslint'],
 
-  extends: [
-    'plugin:vue/vue3-recommended',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+	extends: [
+		'plugin:vue/vue3-recommended',
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+	],
 
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 
-    // this rule, if on, would require explicit return type on the `render` function
-    '@typescript-eslint/explicit-function-return-type': 'off',
-  },
+		// this rule, if on, would require explicit return type on the `render` function
+		'@typescript-eslint/explicit-function-return-type': 'off',
 
-  overrides: [
-    {
-      files: ['*.js'],
-      rules: {
-        '@typescript-eslint/no-var-requires': 'off',
-      },
-    },
-  ],
+		'prettier/prettier': [
+			'error',
+			{
+				endOfLine: 'auto',
+			},
+		],
+	},
+
+	overrides: [
+		{
+			files: ['*.js'],
+			rules: {
+				'@typescript-eslint/no-var-requires': 'off',
+			},
+		},
+	],
 }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="./images/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue3 Boilerplate" />
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="./images/logo.png" />
+		<HelloWorld msg="Welcome to Your Vue3 Boilerplate" />
+	</div>
 </template>
 
 <script lang="ts">
@@ -10,7 +10,7 @@ import { defineComponent } from 'vue'
 import HelloWorld from './components/hello-world.vue'
 
 export default defineComponent({
-  name: 'HomePage',
-  components: { HelloWorld },
+	name: 'HomePage',
+	components: { HelloWorld },
 })
 </script>
